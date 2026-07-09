17de37baddea6de33e6964273e0210cdb61721b0
Jonatha-Varjao <jonathavarjao@gmail.com>
2026-07-09 18:48:20 -0300

    chore(api): update alembic env config

--- apps/api/alembic/env.py
+++ apps/api/alembic/env.py
@@ -6,11 +6,14 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from src.app.config import settings
 from src.app.models import Base
 
 config = context.config
 if config.config_file_name is not None and not config.attributes.get("skip_logging", False):
     fileConfig(config.config_file_name)
+
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 target_metadata = Base.metadata
 
