9b4c70a1644eedcfaa174bf444e72ca611971749
Jonatha-Varjao <jonathavarjao@gmail.com>
2026-07-09 18:48:44 -0300

    refactor(web): update chat layout

--- apps/web/src/pages/ChatLayout.tsx
+++ apps/web/src/pages/ChatLayout.tsx
@@ -6,11 +6,11 @@ export default function ChatLayout() {
   return (
     <div className="flex h-screen bg-white dark:bg-gray-900">
       <Sidebar />
-      <main className="flex flex-1 flex-col">
+      <main className="flex flex-1 flex-col min-h-0">
         <header className="flex items-center justify-end border-b border-gray-200 px-4 py-2 dark:border-gray-700">
           <ThemeToggle />
         </header>
-        <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col min-h-0">
           <Outlet />
         </div>
       </main>
