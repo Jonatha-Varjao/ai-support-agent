f2a4b5c48a826f951b3ee016ea16bc4682bd3d50
Jonatha-Varjao <jonathavarjao@gmail.com>
2026-07-09 18:48:43 -0300

    chore(web): update app entry point

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { STALE_30S } from "./lib/constants";
 import App from "./App";
 import "./index.css";
 
 const queryClient = new QueryClient({
-  defaultOptions: { queries: { staleTime: 30_000 } },
+  defaultOptions: { queries: { staleTime: STALE_30S } },
 });
 
 createRoot(document.getElementById("root")!).render(
